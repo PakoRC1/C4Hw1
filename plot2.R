abcc2b0e1f69d2e8ea210974bbfdc9b215f7b821
PakoRC1 <franciscodaniel.ramirez@hotmail.com>
2017-02-26 15:08:27 -0600

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,22 @@
+library(data.table)
+if(!file.exists("H1"))
+{
+  dir.create("H1")
+}
+
+if(!file.exists("./H1/data.zip"))
+{
+  fileUrl<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(fileUrl, destfile="./H1/data.zip")
+  dateDownloaded<-date() #"Sun Feb 26 13:21:31 2017"
+}
+
+data<-unz("./H1/data.zip", "household_power_consumption.txt")
+data2<-data.table(read.table(data,sep=";",header=TRUE, na.strings="?",stringsAsFactors = F))
+DT<-data2[data2$Date == "1/2/2007" | data2$Date == "2/2/2007"]
+DT[,datehour:=strptime(paste(DT$Date,DT$Time),"%d/%m/%Y %H:%M:%S")]
+
+
+with(DT,plot(datehour,Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)"))
+dev.copy(png,"./H1/plot2.png") #genera archivo png con la gráfica que se genera en la pantalla
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,29 @@
+library(data.table)
+if(!file.exists("H1"))
+{
+  dir.create("H1")
+}
+
+if(!file.exists("./H1/data.zip"))
+{
+  fileUrl<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(fileUrl, destfile="./H1/data.zip")
+  dateDownloaded<-date() #"Sun Feb 26 13:21:31 2017"
+}
+
+data<-unz("./H1/data.zip", "household_power_consumption.txt")
+data2<-data.table(read.table(data,sep=";",header=TRUE, na.strings="?",stringsAsFactors = F))
+DT<-data2[data2$Date == "1/2/2007" | data2$Date == "2/2/2007"]
+DT[,datehour:=strptime(paste(DT$Date,DT$Time),"%d/%m/%Y %H:%M:%S")]
+
+windows()
+with(DT,plot(datehour,Sub_metering_1,xlab="",ylab="Energy sub metering",type="n"))
+with(DT,lines(datehour,Sub_metering_1,col="black"))
+with(DT,lines(datehour,Sub_metering_2,col="red"))
+with(DT,lines(datehour,Sub_metering_3,col="blue"))
+legend("topright",lty=c(1,1,1),col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex=0.6,
+       y.intersp = 0.8,x.intersp = 0.5)
+
+dev.copy(png,"./H1/plot3.png") #genera archivo png con la gráfica que se genera en la pantalla
+dev.off()
+
